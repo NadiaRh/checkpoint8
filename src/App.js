201c5e4101413de54827a2e69cd94c6e958a7967
NadiaRh <nadia.rahmeni@yahoo.com>
2020-03-28 15:04:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import daniel from "./daniel.jpg";
+import harold from './harold.jpg';
+import sarah from './sarah.jpg';
 
-function App() {
+const ResultDaniel = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img style={{width:300}} src={daniel} alt=""></img>
+      <p>I am a highly competent iT professional with a proven track record in designing websites,networking and managing databases.</p>
+    </div>
+  );
+};
+
+const ResultHarold = () => {
+  return (
+    <div>
+      <img style={{width:400}} src={harold} alt=""></img>
+      <p>I am a hardworking and ambitious individual with a great passion for the transport and logistics industry.</p>
     </div>
   );
 }
 
-export default App;
+const ResultSarah = () => {
+  return (
+    <div>
+      <img  style={{width:500 }} src={sarah} alt=""></img>
+     <p> I have a clear, logical mind with a practical approach to problem-solving and a drive te see things through to completion. I have more than 2 years of experience in managing and leading teams across multiple sectors.</p>
+    </div>
+  );
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayedComponent: 'Daniel'
+    };
+  }
+
+  buttonClickHandler (button) {
+    this.setState({
+      displayedComponent: button
+    });
+  }
+
+  getDisplayedComponent () {
+    let {displayedComponent} = this.state;
+
+    if (displayedComponent === 'Harold') {
+      return (
+        <ResultHarold />
+      );
+    }
+
+    if (displayedComponent === 'Sarah') {
+      return (
+        <ResultSarah />
+      );
+    }
+
+    return (<ResultDaniel />);
+  }
+
+
+  render () {
+    return (
+      <div> 
+        <input type="button" value=" Daniel" onClick={this.buttonClickHandler.bind(this, 'Daniel')} />
+        <input type="button" value=" Harold" onClick={this.buttonClickHandler.bind(this, 'Harold')} />
+        <input type="button" value=" Sarah" onClick={this.buttonClickHandler.bind(this, 'Sarah')}/>
+
+        {this.getDisplayedComponent()}
+      </div>
+    );
+  }
+}
+
+export default Main;
